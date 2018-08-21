e67c3502826c3922d0c4278140408f85292d4681
wrj.s <wrj.s@wrjs.local>
2018-08-21 12:07:26 -0700

    add vc_memalloc.c, vc_strjoin.c(not finally)

---
+++ vc_memalloc.c
@@ -0,0 +1,35 @@
+/* ************************************************ */
+/*                                                   */
+/* vc_strclr.c                                       */
+/*                                                  */
+/* By: Team D - Alex, Chin, Giada, Suelen, Yukako   */
+/*                                                    */
+/* ************************************************ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void vc_bzero(void *s, size_t n)
+{
+    char *c = s;
+    size_t i;
+
+    for (i = 0; i < n; ++i)
+    {
+        c[i] = '\0';
+    }
+}
+
+void *vc_memalloc(size_t size)
+{
+    void *mem;
+
+    if (size == 0)
+        return (NULL);
+    if ((mem = (void *)malloc(size)) == NULL)
+        ;
+    return (NULL);
+
+    vc_bzero(mem, size);
+    return (mem);
+}
