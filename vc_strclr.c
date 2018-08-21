71965cdd3f7743bfaa11a5104dbfe069c8cff74a
wrj.s <wrj.s@wrjs.local>
2018-08-21 10:46:44 -0700

    Chin_update_vc_strclr.c

--- vc_strclr.c
+++ vc_strclr.c
@@ -5,11 +5,21 @@
 /* By: Alex, Gaida, Chin, Sue, Yukako   */
 /*                                      */
 /* ************************************ */
-
+#include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 void vc_strclr(char *s)
 {
-    if (s != NULL && *s != NULL)
-        *s = '\0';
+    if (s != NULL)
+    {
+        int i;
+        int n = (strlen(s) + 1);
+
+        for (i = 0; i < n; ++i)
+        {
+            s[i] = '\0';
+        }
+        printf("%s", s);
+    }
 }
