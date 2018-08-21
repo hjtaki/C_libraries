5f86cd3a5521b4c63703da9b1664e3873c52edc6
suelentanaka <tanakasuelen@gmail.com>
2018-08-21 12:27:46 -0700

    add memcmp.c

---
+++ memcmp.c
@@ -0,0 +1,22 @@
+/*                                          	  */
+/* vc_memcmp.c                                    */
+/*                                                */
+/* By: Team D - Alex, Chin, Giada, Suelen, Yukako */
+/*                                                */
+/* ********************************************** */
+#include <stdio.h>
+
+int vc_memcmp(const char *s1, const char *s2, size_t n)
+{
+    unsigned char u1, u2;
+    for (; n--; s1++, s2++)
+    {
+        u1 = *(unsigned char *)s1;
+        u2 = *(unsigned char *)s2;
+        if (u1 != u2)
+        {
+            return (u1 - u2);
+        }
+    }
+    return 0;
+}
