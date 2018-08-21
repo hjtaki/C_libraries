0e3b734b0a3fefc226e811d957f9bae3b0b532d1
Giada <cpupi93@gmail.com>
2018-08-21 09:38:04 -0700

    i deleted the main() part

--- vc_memset.c
+++ vc_memset.c
@@ -1,45 +1,19 @@
 /* MEMSET */
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void vc_memset(void *ptr, int value, int size)
 {
-    if(ptr != NULL && size > 0)
+    if (ptr != NULL && size > 0)
     {
         unsigned char *temp = ptr;
         int i = 0;
 
-        for( i = 0; i < size; i++)
+        for (i = 0; i < size; i++)
         {
             *temp++ = (unsigned char)value;
         }
     }
-
-}
-
-int main()
-
-{
-    char *ptr = NULL;
-    char *ptr1 = NULL;
-    ptr = (char *)malloc(8);
-    ptr1 = (char*)malloc(8);
-
-    printf("ptr after memset\n");
-
-    vc_memset(ptr,'A',sizeof(ptr));
-
-    puts(ptr);
-    printf("ptr1 after memset\n");
-
-    vc_memset(ptr1,'C',sizeof(ptr1));
-
-    puts(ptr1);
-    free(ptr);
-    free(ptr1);
-
-    return 0;
-
 }
