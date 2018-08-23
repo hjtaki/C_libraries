b2c702bd96a3fb788b18aa3440e7621d9e3f19b7
wrj.s <wrj.s@wrjs.local>
2018-08-23 09:54:54 -0700

    update vc_putnbr (delete unnecessary function)

--- vc_putnbr.c
+++ vc_putnbr.c
@@ -8,16 +8,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int vc_strlen(char *s)
-{
-    int i = 0;
-    while (*(s + i) != '\0')
-    {
-        i++;
-    }
-    return i;
-}
-
 void vc_putnbr(int *n)
 {
     write(1, n, sizeof(n));
