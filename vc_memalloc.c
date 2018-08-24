5ab3a56ea4d0acabe8d0b30373594155ecc8a16d
wrj.s <wrj.s@wrjs.local>
2018-08-24 10:08:16 -0700

    fix(vc_memalloc)

--- vc_memalloc.c
+++ vc_memalloc.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ********************************************** */
 /*                                                */
 /* vc_memalloc.c                                  */
@@ -7,30 +6,6 @@
 /*                                                */
 /* ********************************************** */
 #include "libvc.h"
-
-static void vc_bzero(void *s, size_t n)
-{
-    char *c = s;
-    size_t i;
-
-    for (i = 0; i < n; ++i)
-    {
-        c[i] = '\0';
-    }
-}
-=======
-/* ************************************************ */
-/*                                                  */
-/* vc_memalloc.c                                      */
-/*                                                  */
-/* By: Team D - Alex, Chin, Giada, Suelen, Yukako   */
-/*                                                  */
-/* ************************************************ */
-
-#include <stdio.h>
-#include <stdlib.h>
-
->>>>>>> f6acc42da332f51fc24f168c1acfcadc92e88a35
 
 void *vc_memalloc(size_t size)
 {
@@ -44,14 +19,8 @@ void *vc_memalloc(size_t size)
     }
 }
 
-<<<<<<< HEAD
-    vc_bzero(fresh, size);
-    return (fresh);
-}
-=======
 int main()
 {
     int size = 50;
     vc_memalloc(size);
 }
->>>>>>> f6acc42da332f51fc24f168c1acfcadc92e88a35
